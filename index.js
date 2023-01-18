c146ab51dfa7a3eeae0204086029cc2e3383338e
leonardopadilha <leonardopadilha28@gmail.com>
2023-01-18 06:57:59 -0300

    Instação Jest

--- index.js
+++ index.js
@@ -2,50 +2,7 @@ const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtra
 
 const calculaDescontos = (salario, descontos) => salario - descontos;
 
-const verifiqueSe = (valor) => {
-  const assercoes = {
-    ehExatamenteIgualA(esperado) {
-      if (valor !== esperado) {
-        // eslint-disable-next-line no-throw-literal
-        throw {};
-      }
-    },
-  };
-  return assercoes;
+export {
+  somaHorasExtras,
+  calculaDescontos,
 };
-
-const teste = (titulo, funcaoDeTeste) => {
-  try {
-    funcaoDeTeste();
-    console.log(`${titulo} passou`);
-  } catch {
-    console.error(`${titulo} não passou`);
-  }
-};
-
-teste('somaHorasExtras', () => {
-  const esperado = 2500;
-  const retornado = somaHorasExtras(2000, 500);
-
-  verifiqueSe(retornado).ehExatamenteIgualA(esperado);
-});
-
-teste('calculaDesconto', () => {
-  const esperado = 2300;
-  const retornado = calculaDescontos(2500, 200);
-
-  verifiqueSe(retornado).ehExatamenteIgualA(esperado);
-});
-
-/* const teste = (titulo, esperado, retornado) => {
-  if (esperado === retornado) {
-    console.log(`${titulo} passou`);
-  } else {
-    console.error(`${titulo} não passou`);
-  }
-};
-
-teste('somaHorasExtras', 2500, somaHorasExtras(2000, 500));
-teste('calculaDesconto', 2200, calculaDescontos(2500, 300));
-
-*/
