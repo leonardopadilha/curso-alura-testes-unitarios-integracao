e249b2ff628497f016294bc5a9887e5958621119
leonardopadilha <leonardopadilha28@gmail.com>
2023-01-27 05:50:01 -0300

    Ajustes cenário com mock

--- api/src/test/models/editora.test.js
+++ api/src/test/models/editora.test.js
@@ -1,9 +1,9 @@
-import { describe } from "@jest/globals";
+import { describe, expect, it, jest } from "@jest/globals";
 import Editora from "../../models/editora.js";
 
 describe('Testando o modelo Editora', () => {
     const objetoEditora = {
-        nome: 'CD',
+        nome: 'CDC',
         cidade: 'Sao Paulo',
         email: 'c@c.com'
     };
@@ -44,10 +44,24 @@ describe('Testando o modelo Editora', () => {
     it('Deve fazer uma chamada simulada ao banco de dados', () => {
         const editora = new Editora(objetoEditora);
 
-        editora.salvar = () => {
-            console.log('Editora salva no BD')
-        };
+        editora.salvar = jest.fn().mockReturnValue({
+            id: 10,
+            nome: 'CDC',
+            cidade: 'Sao Paulo',
+            email: 'c@c.com',
+            created_at: '2022-10-01',
+            updated_at: '2022-10-01'
+        });
 
-        editora.salvar();
+        const retorno = editora.salvar();
+
+        expect(retorno).toEqual(
+            expect.objectContaining({
+                id: expect.any(Number),
+                ...objetoEditora,
+                created_at: expect.any(String),
+                updated_at: expect.any(String)
+            }),
+        );
     });
 });
