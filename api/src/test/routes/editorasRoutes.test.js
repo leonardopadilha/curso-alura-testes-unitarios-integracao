f4f9529b9a29b12ed5f2afcde7adb939f9603026
leonardopadilha <leonardopadilha28@gmail.com>
2023-01-28 13:00:16 -0300

    Adicionando GET em editoras

--- api/src/test/routes/editorasRoutes.test.js
+++ api/src/test/routes/editorasRoutes.test.js
@@ -1,4 +1,5 @@
 import app from '../../app.js';
+import request from "supertest";
 
 let server;
 
@@ -9,4 +10,16 @@ beforeEach(() => {
 
 afterEach(() => {
     server.close();
+});
+
+describe('GET em /editoras', () => {
+    it('Deve retornar uma lista de editoras', async () => {
+        const resposta = await request(app)
+            .get('/editoras')
+            .set('Accept', 'application/json')
+            .expect('content-type', /json/)
+            .expect(200);
+
+        expect(resposta.body[0].email).toEqual('e@e.com');
+    });
 });
